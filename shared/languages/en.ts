4ae86b4dee02fea7bd37d1a6cf1d7fdd78f68e3f
dmhughes <hughes.dm@gmail.com>
2025-12-23 08:33:26 +0000

    Revert "Merge remote-tracking branch 'upstream/main'"
    This reverts commit 31db406c419497fd52726098d054a1ac22e98c8b, reversing
    changes made to d992859c8210574c2bc1ef9654659540af558cbd.

--- shared/languages/en.ts
+++ shared/languages/en.ts
@@ -1881,5 +1881,4 @@ export const EN = {
    Zoroaster: "Zoroaster",
    Zugspitze: "Zugspitze",
    ZugspitzeDesc: "For each unlocked age, get one point that can be used to provide one extra level to any Great Person that is born from this run. (Storage multipliers are not effective due to this multiplier being dynamic)",
-   EnterExitMinimizedMode: "Enter/Exit Minimized Mode",
 };
